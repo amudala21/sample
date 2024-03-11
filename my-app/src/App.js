0bb2d4e28dc8fb94b0c7a8c863c5bda7fd5095ed
Amudala Reddy Sekhar <sekhar@EXAFLUENCE.LOCAL>
2024-03-11 13:12:49 +0530

    second commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from './home'
+
+import "./App.css"
+
+const App=() =>{
+    return (
+        <>
+        <div className="body">
+            <h1 className="heading">My Account</h1>
+            <Home />
+        </div>
+        
+        </>
+        
+    )
+}
+
+export default App ;
---
+++ my-app/src/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () =>{
+    return (
+        <>
+        <button onMouseOver={console.log("hover ")}>Home Page</button>
+        </>
+    )
+   
+}
+export default Home;
+
